4c614519af68fd04d12c98ee998693f0db98b722
minhyeok kweun <kmh910924@gmail.com>
2017-06-02 11:30:04 +0900

    implement update() for the epicenter map; temporarily disable the brush

--- src/js/barchart.js
+++ src/js/barchart.js
@@ -3,12 +3,12 @@ var descriptionBarChart = 'DESCRIPTION: This bar chart shows how the occurrence 
     'From this visualization you can easily find the number of occurrences dramatically changed in 2016 with an earthquake largest in scale.';
 
 // Default configuration to set up a bar chart.
-var config = {}
-config['frame'] = { 'width': 400, 'height': 300 } // The size of the frame in HTML doc.
-config['margin'] = { top: 20, right: 20, bottom: 50, left: 50 }
-config['chart'] = {
-    'width': config.frame.width - config.margin.left - config.margin.right,
-    'height': config.frame.height - config.margin.top - config.margin.bottom
+var bcConfig = {}
+bcConfig['frame'] = { 'width': 400, 'height': 300 } // The size of the frame in HTML doc.
+bcConfig['margin'] = { top: 20, right: 20, bottom: 50, left: 50 }
+bcConfig['chart'] = {
+    'width': bcConfig.frame.width - bcConfig.margin.left - bcConfig.margin.right,
+    'height': bcConfig.frame.height - bcConfig.margin.top - bcConfig.margin.bottom
 }
 
 // compute statistics (# of occurrences per year) from the given records.
@@ -36,46 +36,46 @@ function getStatistics(records, fromYear, toYear) {
 
 // shows yearly statistics of occurrence of earthquakes in a bar chart.
 function setupBarChart() {
-    config.x = d3.scale.ordinal().rangeRoundBands([0, config.chart.width], .05);
-    config.y = d3.scale.linear().range([config.chart.height, 0]);
-    config.x.domain([]);
-    config.y.domain([0, 0]);
-    config.axis_x = d3.svg.axis().scale(config.x).orient('bottom');
-    config.axis_y = d3.svg.axis().scale(config.y).orient('left').ticks(10);
+    bcConfig.x = d3.scale.ordinal().rangeRoundBands([0, bcConfig.chart.width], .05);
+    bcConfig.y = d3.scale.linear().range([bcConfig.chart.height, 0]);
+    bcConfig.x.domain([]);
+    bcConfig.y.domain([0, 0]);
+    bcConfig.axis_x = d3.svg.axis().scale(bcConfig.x).orient('bottom');
+    bcConfig.axis_y = d3.svg.axis().scale(bcConfig.y).orient('left').ticks(10);
 
-    config.svg = d3.select('#yearly-statistics').append('svg')
-        .attr('width', config.frame.width)
-        .attr('height', config.frame.height)
+    bcConfig.svg = d3.select('#yearly-statistics').append('svg')
+        .attr('width', bcConfig.frame.width)
+        .attr('height', bcConfig.frame.height)
         .append('g')
-        .attr('transform', translate(config.margin.left, config.margin.top));
+        .attr('transform', translate(bcConfig.margin.left, bcConfig.margin.top));
 
-    config.svg.append('g') // config the X axis 
+    bcConfig.svg.append('g') // bcConfig the X axis 
         .attr('class', 'x axis')
-        .attr('transform', translate(0, config.chart.height))
-        .call(config.axis_x)
+        .attr('transform', translate(0, bcConfig.chart.height))
+        .call(bcConfig.axis_x)
         .selectAll('text')
         .style('text-anchor', 'end')
         .attr('dx', '-.8em')
         .attr('dy', '-.55em')
         .attr('transform', 'rotate(-90)');
 
-    config.svg.append('text') // text label for the X axis
-        .attr('transform', translate((config.chart.width / 2), (config.chart.height + config.margin.bottom)))
+    bcConfig.svg.append('text') // text label for the X axis
+        .attr('transform', translate((bcConfig.chart.width / 2), (bcConfig.chart.height + bcConfig.margin.bottom)))
         .style('text-anchor', 'middle')
         .text('Year')
         .attr('font-size', 14);
 
-    config.svg.append('g') // config the Y axis
+    bcConfig.svg.append('g') // bcConfig the Y axis
         .attr('class', 'y axis')
-        .call(config.axis_y)
+        .call(bcConfig.axis_y)
         .append('text')
         .attr('transform', 'rotate(-90)')
         .attr('dy', '.71em');
 
-    config.svg.append('text') // text label for the Y axis
+    bcConfig.svg.append('text') // text label for the Y axis
         .attr('transform', 'rotate(-90)')
-        .attr('y', 0 - config.margin.left)
-        .attr('x', 0 - (config.chart.height / 2))
+        .attr('y', 0 - bcConfig.margin.left)
+        .attr('x', 0 - (bcConfig.chart.height / 2))
         .attr('dy', '1em')
         .style('text-anchor', 'middle')
         .text('#(Occurrence)')
@@ -87,39 +87,39 @@ function updateBarChart(records, fromYear, toYear) {
     var data = getStatistics(records, fromYear, toYear);
 
     // update the domain of the X and Y axis to reflect change in statistics.
-    config.x.domain(data.map((d) => d[0]));
-    config.y.domain([0, d3.max(data, (d) => d[1])]);
+    bcConfig.x.domain(data.map((d) => d[0]));
+    bcConfig.y.domain([0, d3.max(data, (d) => d[1])]);
 
-    var bars = config.svg.selectAll("rect").data(data, (d) => d[0]);
+    var bars = bcConfig.svg.selectAll("rect").data(data, (d) => d[0]);
 
-    config.svg.select(".x.axis") // update the X axis
+    bcConfig.svg.select(".x.axis") // update the X axis
         .transition().duration(200).ease("sin-in-out")
-        .call(config.axis_x)
+        .call(bcConfig.axis_x)
         .selectAll('text')
         .style('text-anchor', 'end')
         .attr('dx', '-.8em')
         .attr('dy', '-.55em')
         .attr('transform', 'rotate(-90)');
 
-    config.svg.select(".y.axis") // update the Y axis
+    bcConfig.svg.select(".y.axis") // update the Y axis
         .transition().duration(200).ease("sin-in-out")
-        .call(config.axis_y);
+        .call(bcConfig.axis_y);
 
     // update bars in the chart.
     bars.exit().remove();
 
     //
     bars.transition().duration(200)
-        .attr('x', (d) => config.x(d[0]))
-        .attr('width', config.x.rangeBand())
-        .attr('y', (d) => config.y(d[1]))
-        .attr('height', (d) => (config.chart.height - config.y(d[1])))
+        .attr('x', (d) => bcConfig.x(d[0]))
+        .attr('width', bcConfig.x.rangeBand())
+        .attr('y', (d) => bcConfig.y(d[1]))
+        .attr('height', (d) => (bcConfig.chart.height - bcConfig.y(d[1])))
         .attr('fill', 'steelblue');
 
     bars.enter()
         .append('rect')
-        .attr('x', (d) => config.x(d[0]))
-        .attr('width', config.x.rangeBand())
+        .attr('x', (d) => bcConfig.x(d[0]))
+        .attr('width', bcConfig.x.rangeBand())
         .style('opacity', 0)
         .on('mouseover', function(d, i) {
             console.log(d[0] + ", " + d[1]);
@@ -132,8 +132,8 @@ function updateBarChart(records, fromYear, toYear) {
             d3.select(this).style("fill", "steelblue");
         })
         .transition().duration(200)
-        .attr('y', (d) => config.y(d[1]))
-        .attr('height', (d) => (config.chart.height - config.y(d[1])))
+        .attr('y', (d) => bcConfig.y(d[1]))
+        .attr('height', (d) => (bcConfig.chart.height - bcConfig.y(d[1])))
         .style('opacity', 1)
         .style('fill', 'steelblue');
 }
--- src/js/scatterplot.js
+++ src/js/scatterplot.js
@@ -4,92 +4,80 @@ var descriptionEpicenterMap = 'DESCRIPTION: This map shows epicenters of earthqu
     'The radius of each circle also increases with its magnitude. ' +
     'Additionally, you can see the statistics of earthquakes which have occurred in a region by using brush.';
 
-var config_EpicenterMap = {};
-config_EpicenterMap['frame'] = { 'width': 550, 'height': 750 }; // The size of the frame in HTML doc.
-config_EpicenterMap['plot'] = { 'width': 439, 'height': 638 };
-config_EpicenterMap['margin'] = { left: 50, bottom: 50 };
-config_EpicenterMap['longitude'] = { left: 122, right: 132 };
-config_EpicenterMap['latitude'] = { top: 43, bottom: 32 };
+var emConfig = {};
+emConfig['frame'] = { 'width': 550, 'height': 750 }; // The size of the frame in HTML doc.
+emConfig['plot'] = { 'width': 439, 'height': 638 };
+emConfig['margin'] = { left: 50, bottom: 50 };
+emConfig['longitude'] = { left: 122, right: 132 };
+emConfig['latitude'] = { top: 43, bottom: 32 };
 
 // 주어진 레코드에서 진앙의 위치를 읽어 위-경도 plot에 dot형태로 출력한다. 
 // 규모별로 dot의 색깔과 크기를 지정하는 룰을 전달할 수 있다 (colorRule & radiusRule, respectively).
-function showEpicenterMap(records, colorRule, radiusRule) {
+function setupEpicenterMap(colorRule, radiusRule) {
+    emConfig.svg = d3.select('#epicenter-plot').append('svg')
+        .attr('width', emConfig.frame.width)
+        .attr('height', emConfig.frame.height);
 
-    var config = config_EpicenterMap;
-    var data = records;
+    emConfig.x = d3.scale.linear()
+        .domain([emConfig.longitude.left, emConfig.longitude.right]) // longitude
+        .range([emConfig.margin.left, emConfig.plot.width + emConfig.margin.left]);
 
-    var svg = d3.select('#epicenter-plot').append('svg')
-        // .attr('id', 'epicenter-plot')
-        .attr('width', config.frame.width)
-        .attr('height', config.frame.height);
+    emConfig.y = d3.scale.linear()
+        .domain([emConfig.latitude.bottom, emConfig.latitude.top])
+        .range([emConfig.plot.height, 0]);
 
-    var x = d3.scale.linear()
-        .domain([config.longitude.left, config.longitude.right]) // longitude
-        .range([config.margin.left, config.plot.width + config.margin.left]);
+    emConfig.colorRule = colorRule;
+    emConfig.radiusRule = radiusRule;
 
-    var y = d3.scale.linear()
-        .domain([config.latitude.bottom, config.latitude.top])
-        .range([config.plot.height, 0]);
-
-    svg
-        .selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('r', (d) => determineRadius(d.magnitude, radiusRule))
-        .attr('cx', (d) => x(d.longitude.value))
-        .attr('cy', (d) => y(d.latitude.value))
-        .style('fill', (d) => determineColor(d.magnitude, colorRule))
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
+    emConfig.axis_x = d3.svg.axis()
+        .scale(emConfig.x)
         .orient('bottom');
 
-    svg
+    emConfig.svg
         .append('g')
         .attr('class', 'x axis')
-        .attr('transform', translate(0, config.plot.height))
-        .call(xAxis);
+        .attr('transform', translate(0, emConfig.plot.height))
+        .call(emConfig.axis_x);
 
-    svg.append('text') // text label for the x axis
-        .attr('transform', translate((config.plot.width + config.margin.left) / 2, (config.plot.height + config.margin.bottom)))
+    emConfig.svg.append('text') // text label for the x axis
+        .attr('transform', translate((emConfig.plot.width + emConfig.margin.left) / 2, (emConfig.plot.height + emConfig.margin.bottom)))
         .style('text-anchor', 'middle')
         .text('Longitude')
         .attr('font-size', 18);
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
+    emConfig.axis_y = d3.svg.axis()
+        .scale(emConfig.y)
         .orient('left');
 
-    svg
+    emConfig.svg
         .append('g')
         .attr('class', 'y axis')
-        .attr('transform', translate(config.margin.left, 0))
-        .call(yAxis);
+        .attr('transform', translate(emConfig.margin.left, 0))
+        .call(emConfig.axis_y);
 
-    svg.append('text') // text label for the y axis
+    emConfig.svg.append('text') // text label for the y axis
         .attr('transform', 'rotate(-90)')
         .attr('y', 0)
-        .attr('x', 0 - (config.plot.height / 2))
+        .attr('x', 0 - (emConfig.plot.height / 2))
         .attr('dy', '1em')
         .style('text-anchor', 'middle')
         .text('Latitude')
         .attr('font-size', 18);
 
-    var brush = d3.svg.brush()
-        .x(x)
-        .y(y)
+    emConfig.brush = d3.svg.brush()
+        .x(emConfig.x)
+        .y(emConfig.y)
         .on('brush', update)
         .on('brushend', update)
 
     function update() {
-        var extent = brush.extent();
+        var extent = emConfig.brush.extent();
         var widthRange = [extent[0][0], extent[1][0]];
         var lengthRange = [extent[0][1], extent[1][1]];
         var magnitudeSum = 0,
             n = 0;
 
-        svg
+        emConfig.svg
             .selectAll('circle')
             .style('opacity', 0.5)
             .filter((d) => (widthRange[0] <= d.longitude.value && d.longitude.value <= widthRange[1] &&
@@ -103,17 +91,48 @@ function showEpicenterMap(records, colorRule, radiusRule) {
         if (n > 0) {
             d3.select('#mean-magnitude').text(d3.format('.2f')(magnitudeSum / n));
             d3.select('#num-occurrences').text(d3.format('f')(n));
-            console.log("mean-magnitude: " + (magnitudeSum / n));
-            console.log("num-occurance: " + (n));
         }
     }
 
-    svg
-        .append('g')
-        .attr('class', 'brush')
-        .call(brush)
+    // temporarily disable the brush
+    // emConfig.svg
+    //     .append('g')
+    //     .attr('class', 'brush')
+    //     .call(emConfig.brush)
 
-    //document.getElementById('description').textContent = descriptionEpicenterMap;
+}
+
+function updateEpicenterMap(records) {
+    var circles = emConfig.svg
+        .selectAll('circle')
+        .data(records, function(d, i) { return i; });
+
+    circles.exit().transition().duration(200).remove();
+
+    circles.transition().duration(200)
+        .attr('r', (d) => determineRadius(d.magnitude, emConfig.radiusRule))
+        .attr('cx', (d) => emConfig.x(d.longitude.value))
+        .attr('cy', (d) => emConfig.y(d.latitude.value))
+        .style('fill', (d) => determineColor(d.magnitude, emConfig.colorRule));
+
+    circles.enter()
+        .append('circle')
+        .attr('r', (d) => determineRadius(d.magnitude, emConfig.radiusRule))
+        .attr('cx', (d) => emConfig.x(d.longitude.value))
+        .attr('cy', (d) => emConfig.y(d.latitude.value))
+        .style('fill', (d) => determineColor(d.magnitude, emConfig.colorRule))
+        .on('mouseover', function(d, i) {
+            var date = d.occurred_date.year.toString() +
+                "." + d.occurred_date.month.toString() +
+                "." + d.occurred_date.day.toString() +
+                " " + d.occurred_date.hour.toString() +
+                ":" + d.occurred_date.minute.toString();
+            d3.select(this)
+                .append("svg:title")
+                .text((d) => ("시간: " + date + "\n" +
+                    "위치: " + d.latitude.value + d.latitude.direction + ", " + d.longitude.value + d.longitude.direction +
+                    "\n규모: " + d.magnitude));
+        });
 }
 
 // magnitude에 따라 점의 색깔을 결정한다.

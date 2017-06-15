def1348d4e0f971c9e4263662fb053a7ff1300bd
bellatoris <bellatoris@snu.ac.kr>
2017-06-15 10:21:51 +0900

    [earthquakeMap] modify css

--- src/js/piechart.js
+++ src/js/piechart.js
@@ -13,9 +13,9 @@ function pieChart(id, statistics) {
         .text("a simple tooltip");
 
     console.log(statistics);
-    var width = 250;
-    var height = 250;
-    var radius = Math.min(width, height)/2;
+    var width = 300;
+    var height = 300;
+    var radius = Math.min(width, height) / 2;
 
     pieConfig.svg = d3.select("#pie-charts").append('svg')
         .attr("width", pieConfig.dimension.width)
@@ -178,52 +178,4 @@ function pieChart(id, statistics) {
     change(statistics);
 
     return change;
-
-    // pieConfig.arc = d3.svg.arc()
-    //     .outerRadius(pieConfig.demension.radius - 10)
-    //     .innerRadius(0);
-    //
-    // pieConfig.label = d3.svg.arc()
-    //     .outerRadius(pieConfig.demension.radius - 40)
-    //     .innerRadius(pieConfig.demension.radius - 40);
-    //
-    // pieConfig.g = pieConfig.svg.selectAll("arc")
-    //     .data(function() {
-    //         return pieConfig.pie(statistics);
-    //     }).enter().append("g")
-    //     .attr("class", "arc");
-    //
-    // pieConfig.g.append("path")
-    //     .attr("d", pieConfig.arc)
-    //     .each(function(d) { this._current = d; })
-    //     .attr("fill", function(d) { return pieConfig.colorVec[key2Index(d.data[0])]; })
-    //     .attr("opacity", 0.7)
-    //     .on('mouseover', function(d, i) {
-    //         console.log(d.data[0] + ", " + d.data[1]);
-    //         tooltip.style("visibility", "visible");
-    //         d3.select(this)
-    //             .style("opacity", 1);
-    //         emphasizeRecords(function(rec) {
-    //             if (d.data[0] === '1-3') {
-    //                 return (1 <= rec.magnitude && rec.magnitude < 3);
-    //             } else if (d.data[0] === '3-4') {
-    //                 return (3 <= rec.magnitude && rec.magnitude < 4);
-    //             } else if (d.data[0] === '4-5') {
-    //                 return (4 <= rec.magnitude && rec.magnitude < 5);
-    //             } else {
-    //                 return (5 <= rec.magnitude && rec.magnitude <= 6);
-    //             }
-    //         });
-    //     })
-    //     .on("mousemove", function(d) {
-    //         tooltip.text(d.data[0] + ", " + d.data[1]);
-    //         tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px")
-    //             .style("background-color", "skyblue").style("font-size", "24px");
-    //     })
-    //     .on("mouseout", function() {
-    //         d3.select(this).style("opacity", 0.7);
-    //         tooltip.style("visibility", "hidden");
-    //         emphasizeRecords((rec) => true);
-    //     });;
-
 }
